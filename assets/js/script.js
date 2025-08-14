5d4674af056a28c701d1c725d0de14708c5e22e4
VK1534 <vineethk77v@gmail.com>
2025-08-14 13:47:08 +0530

    Added scripts for form validation, toggle sidebar. Added article to html page sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,45 @@
+'use strict';
+
+// Sidebar & Topbar Loaders
+
 fetch('sidebar.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('sidebar-container').innerHTML = data;
-      });
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('sidebar-container').innerHTML = data;
+  });
 
 fetch('topbar.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('topbar-container').innerHTML = data;
-      });
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('topbar-container').innerHTML = data;
+  });
+
+
+// Sidebar Toggle
+
+const elementToggleFunc = function (elem) { 
+  elem.classList.toggle("active"); 
+};
+
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+sidebarBtn.addEventListener("click", function () { 
+  elementToggleFunc(sidebar); 
+});
+
+
+//Form Validation
+  
+const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
+const formBtn = document.querySelector("[data-form-btn]");
+
+for (let i = 0; i < formInputs.length; i++) {
+  formInputs[i].addEventListener("input", function () {
+    if (form.checkValidity()) {
+      formBtn.removeAttribute("disabled");
+    } else {
+      formBtn.setAttribute("disabled", "");
+    }
+  });
+}
